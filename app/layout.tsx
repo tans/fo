5655725f79a7fc1d20b8ac1c6aa766b2a3508638
tans <tans@imac.local>
2024-08-19 19:53:40 +0800

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "般若田舍",
-  description: "线上道场。每日一起念经,参禅,悟道。",
+  description: "线上道场。一起念经,参禅,悟道。",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
         般若·田舍
       </div>
 
-      <div className="text-center my-4">线上道场。每日一起念经,参禅,悟道。</div>
+      <div className="text-center my-4">
+        般若田舍线上道场 - 一起念经,参禅,悟道。
+      </div>
       <div className="text-center my-4">
         <a href="/me" className="underline font-black">
           《心经》
