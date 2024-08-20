05a1f71369fb3027ab46585a8b78a255e38ba289
tans <tans@imac.local>
2024-08-20 10:10:29 +0800

    -

---
+++ components/app/copy-button.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+export default function ({ text, children }) {
+  return (
+    <>
+      <CopyToClipboard text={text}>
+        <Button
+          size="sm"
+          onClick={() => {
+            toast({ title: "复制成功", duration: 1000 });
+          }}
+        >
+          {children}
+        </Button>
+      </CopyToClipboard>
+    </>
+  );
+}
